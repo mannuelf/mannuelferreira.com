312f2fa589c561911b8b669dd6b288dcfacfbad2
Manny <Mannuel Ferreira>
2016-08-21 01:52:16 +0200

    some copy and fonts n colours

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,10 @@
       <div class="container">
         <div class="colourblocking"></div>
         <div class="title-container">
-            <div class="title title--firaSans ghostWhite">Mannuel Ferreira</div>
+            <h1 class="title title--firaSans ghostWhite">Mannuel Ferreira</h1>
+            <p class="copy copy--droid">
+              Hi, I'm Web developer &amp; designer. This is my home on the web where I try out web technologies and ramble on about design and technology.
+            </p>
         </div>
       </div>
     </body>
