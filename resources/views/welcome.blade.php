0837bc5bb28a1666edd66e9b21e5382cb2858974
Manny <Mannuel Ferreira>
2016-08-21 00:38:48 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Mannuel Ferreira</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="assets/css/app.css">
+    </head>
+    <body>
+      <div class="photograph">
+        <img src="/assets/img/photo_large.jpg" alt="Mannuel Ferreira" />
+      </div>
+      <div class="container">
+          <div class="content">
+              <div class="title">Mannuel Ferreira</div>
+          </div>
+      </div>
+    </body>
+</html>
