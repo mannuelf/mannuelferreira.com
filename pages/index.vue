691c6019e73c73f0b822272ddec654e813c70373
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-28 13:04:35 +0200

    added editor config and updated copy

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,12 @@
             <h1>Hello world!</h1>
             
             <h2>My name is Mannuel Ferreira</h2>
-            <p>Welcome to my site, I work at <a href="https://superbalist.com">Superbalist.com</a> as a Software Engineer, I write on a blog I created <a href="http://www.themwebs.me">www.themwebs.me</a></p>
+            <p>Welcome to my site, I work at <a href="https://superbalist.com">Superbalist.com</a> as a Software Engineer</p>
+            
+            <p>I write on a blog here <a href="http://www.themwebs.me">www.themwebs.me</a></p>
             
             <h3>What I do</h3>
-            <p>I enjoy building and designing applications for the web. I enjoy working with web technologies such as PHP, python, Node.js, Sass (Sass syntax), JavaScript. I enjoy learning and teaching web development and engineering concepts. I enjoy sportsing football and squash.</p>
+            <p>I enjoy building and designing applications for the web. I enjoy working with web technologies such as PHP, python, Node.js, Sass (Sass syntax), JavaScript. I enjoy learning and teaching web development and engineering concepts. I enjoy sports'ing football and squash.</p>
             <p>I recently launched a site called <a href="https://www.whatcoinwhichcoin.com">WHATcoin WHICHcoin</a>. It's a work in progress, you can view the latest bitcoin and etherium price there.</p>
             
             <h3>Follow me</h3>
