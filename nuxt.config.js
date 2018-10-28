c65be5ed2960e77599650b7275227d2a6ef4c1a2
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-28 11:13:55 +0200

    update about and meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'description', content: 'Mannuel Ferreira, Software Engineer' }
+      { name: 'description', content: 'Mannuel Ferreira, Software Engineer. I enjoy building and designing applications for the web. I enjoy working with web technologies such as PHP, python, Node.js, Sass (Sass syntax), JavaScript' }
     ]
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
             <p>Welcome to my site, I work at <a href="https://superbalist.com">Superbalist.com</a> as a Software Engineer, I write on a blog I created <a href="http://www.themwebs.me">www.themwebs.me</a></p>
             
             <h3>What I do</h3>
-            <p>I enjoy building and designing applications for the web. I enjoy working with web technologies, I enjoy learning and teaching, I enjoy sportsing football and squash.</p>
+            <p>I enjoy building and designing applications for the web. I enjoy working with web technologies such as PHP, python, Node.js, Sass (Sass syntax), JavaScript. I enjoy learning and teaching web development and engineering concepts. I enjoy sportsing football and squash.</p>
             <p>I recently launched a site called <a href="https://www.whatcoinwhichcoin.com">WHATcoin WHICHcoin</a>. It's a work in progress, you can view the latest bitcoin and etherium price there.</p>
             
             <h3>Follow me</h3>
