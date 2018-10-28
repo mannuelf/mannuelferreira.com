806086e5e7d9e805471f2c63eb827fc7ac306578
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-28 13:08:18 +0200

    oops

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'google-site-verification', content='25AFBsBlZv3w387GNKyj0bNBhCjZxdf83TzzBi6X1po' },
+      { name: 'google-site-verification', content: '25AFBsBlZv3w387GNKyj0bNBhCjZxdf83TzzBi6X1po' },
       { name: 'description', content: 'Mannuel Ferreira, Software Engineer. I enjoy building and designing applications for the web. I enjoy working with web technologies such as PHP, python, Node.js, Sass (Sass syntax), JavaScript' }
     ]
   }
