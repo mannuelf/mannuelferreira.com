0fa282b5e98bd81ce55d5bec3b8c6d24cf2f8d7b
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-12-27 21:19:33 +0200

    about page and vue store, installed graphql

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>About me</h1>
+</template>
+<script>
+export default {
+  name: 'AboutMe'
+}
+</script>
+<style lang="sass">
+body
+  background: #eee
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,7 @@
 <template>
   <section class="section">
     <div class="columns is-mobile">
-      <div
-        v-for="(feature, i) of features"
-        :key="i"
-        class="column">
-        <div class="card">
-          <header class="card-header">
-            <p class="card-header-title has-text-grey">
-              {{ feature.title }}
-            </p>
-          </header>
-          <div class="card-content">
-            <div class="content has-text-centered">
-              <b-icon
-                :icon="feature.icon"
-                size="is-large"
-                type="is-primary"/>
-            </div>
-          </div>
-          <footer class="card-footer">
-            <div
-              class="card-footer-item"
-              v-html="feature.content"/>
-          </footer>
-        </div>
-      </div>
+      <p>manny test</p>
     </div>
   </section>
 </template>
