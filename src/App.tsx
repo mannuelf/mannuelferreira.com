10df2e2ac63d13413b75f6f3b062368afa7bc0b0
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-02-22 07:24:39 +0100

    noopener noreferrer

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,26 @@ function App() {
         <article>
           <h1>Hello world!</h1>
           <h2>My name is Mannuel Ferreira</h2>
-          <p>I'm a software engineer, currently working as lecturer teaching Front End development at <a href="https://www.noroff.no/" target="_blank">Noroff Fagskole</a> in Norway.
-            Previously I worked for <a href="https://superbalist.com" target="_blank">Superbalist.com</a>.</p>
-          <p>I write on a blog here <a href="http://www.themwebs.me">www.themwebs.me</a></p>
+          <p>I'm a software engineer, currently working as lecturer teaching Front End development at <a href="https://www.noroff.no/" target="_blank" rel="noopener noreferrer">Noroff Fagskole</a> in Norway.
+            Previously I worked for <a href="https://superbalist.com" target="_blank" rel="noopener noreferrer">Superbalist.com</a>.</p>
+          <p>I write on a blog here <a href="http://www.themwebs.me" target="_blank" rel="noopener noreferrer">www.themwebs.me</a></p>
           <p>
             I enjoy building and designing applications for the web. I enjoy working with web technologies such as
             JavaScript, React, Vue Js, PHP, python, Sass.
             I enjoy learning and teaching web development and engineering concepts. I enjoy sports'ing football and
             squash.
-          </p> <p>
-          I recently launched a site called <a href="https://www.whatcoinwhichcoin.com">WHATcoin WHICHcoin</a>.
-          It's a work in progress, you can view the latest bitcoin and etherium price there.
-        </p></article>
-        <footer><h3>Follow me</h3> <p>on <a href="https://github.com/mannuelf">Github</a></p> <p>on <a
-          href="https://twitter.com/manidf">twitter</a> if you like.</p> <p>on <a
-          href="https://www.linkedin.com/in/mannuelferreira/">linkedin</a> if that's your thing.</p></footer>
+          </p>
+          <p>I recently launched a site called <a href="https://www.whatcoinwhichcoin.com" target="_blank" rel="noopener noreferrer">WHATcoin WHICHcoin</a>.
+          It's a work in progress, you can view the latest bitcoin and etherium price there.</p>
+        </article>
+        <footer>
+          <h3>Follow me</h3>
+          <p>on <a href="https://github.com/mannuelf" target="_blank" rel="noopener noreferrer">Github</a></p>
+          <p>
+            on <a href="https://twitter.com/manidf" target="_blank" rel="noopener noreferrer">twitter</a> if you like.
+          </p>
+          <p>on <a href="https://www.linkedin.com/in/mannuelferreira/" target="_blank" rel="noopener noreferrer">linkedin</a> if that's your thing.</p>
+        </footer>
       </div>
     </div>
   );
