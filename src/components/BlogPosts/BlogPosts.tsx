a77943cc900f7cafd0bc95337b36fd467a65233f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-03-21 19:19:43 +0100

    redux implimented, one api call to themwebs blog

--- src/components/BlogPosts/BlogPosts.tsx
+++ src/components/BlogPosts/BlogPosts.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
 import {getBlogPosts} from '../../redux/actions/posts';
 
 const BlogPosts: any = () => {
-  const dispatch = useDispatch();
+  const dispatch: any = useDispatch();
 
   useEffect(() => {
     dispatch(getBlogPosts());
   }, []);
+
+  const posts: object = useSelector(state => state.posts);
+  if (!posts) return <div>No blog posts today</div>;
 
   return (
     <div>
--- src/redux/actions/posts.ts
+++ src/redux/actions/posts.ts
@@ -2,17 +2,13 @@ import {GET_BLOG} from "../actionTypes";
 import axios from 'axios';
 import {themWebs} from '../../config/ThemWebs';
 
-export const getBlogPosts: any = (payload) => {
+export const getBlogPosts: any = () => {
   return (dispatch) => {
-    return axios.get(`https://themwebs.me/wp-json/wp/v2/posts`)
-      .then(results => {
-        console.log("🕹", results);
+    return axios.get(`${themWebs.api.posts}`).then(results => {
         dispatch({
           type: "GET_BLOG",
-          payload: results
+          payload: results.data
         })
       })
   }
 };
-
-getBlogPosts();
