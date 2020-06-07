cc272af251a54156e8472141d98df2b4e198d424
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-06-07 16:41:54 +0200

    format article component

--- src/components/Articles/ArticlesList.tsx
+++ src/components/Articles/ArticlesList.tsx
@@ -22,28 +22,35 @@ const ArticlesList: any = () => {
   return (
     <section className="app-row py-10">
       <div className="container mx-auto">
-        <h1 className="article-header">Articles</h1>
-        <div dangerouslySetInnerHTML={createMarkup()}></div>
-        {blogPosts.map((post: any): any => {
-          return (
-            <article key={post.id} className="articles-list text-left">
-              <time>{post.modified}</time>
-              <h2
-                className="text-4xl font-semibold"
-                dangerouslySetInnerHTML={createMarkup(post.title.rendered)}
-              ></h2>
-              <p
-                dangerouslySetInnerHTML={createMarkup(post.excerpt.rendered)}
-              ></p>
-              <Link
-                onClick={() => getBlogPost(`${post.id}`)}
-                to={`/articles/${post.id}`}
-              >
-                Read more
-              </Link>
-            </article>
-          );
-        })}
+        <h1 className="article-header text-5xl">Articles</h1>
+        {!blogPosts
+          ? "Loading..."
+          : blogPosts.map((post: any): any => {
+              return (
+                <article
+                  key={post.id}
+                  className="articles-list text-left pb-10"
+                >
+                  <time>{post.modified}</time>
+                  <Link
+                    onClick={() => getBlogPost(`${post.id}`)}
+                    to={`/articles/${post.id}`}
+                  >
+                    <h2
+                      className="text-3xl font-semibold"
+                      dangerouslySetInnerHTML={createMarkup(
+                        post.title.rendered
+                      )}
+                    ></h2>
+                  </Link>
+                  <p
+                    dangerouslySetInnerHTML={createMarkup(
+                      post.excerpt.rendered
+                    )}
+                  ></p>
+                </article>
+              );
+            })}
       </div>
     </section>
   );
