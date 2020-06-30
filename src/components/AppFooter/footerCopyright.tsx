c3b80b602b43863e51f43c9891ba0cb4a458c348
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-06-30 23:39:00 +0200

    footer fixes

--- src/components/AppFooter/footerCopyright.tsx
+++ src/components/AppFooter/footerCopyright.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-const FooterSocial = () => {
+const FooterCopyright = () => {
   return (
-    <div className="w-full sm:w-1/2 md:flex-1 lg:flex-1 text-left pb-10">
-      <h3 className="text-2xl pb-3">Thank you for visiting...</h3>
+    <div className="w-full sm:w-full md:w-full lg:flex-1 xl:flex-1 text-left">
+      <h3 className="text-2xl pb-3">Thank you for visiting</h3>
       <ul className="footer-copyright leading-8">
-        <li className="border-b-2 border-gray-600">
-          Made with <i className="far fa-heart"></i> by Mannuel Ferreira
+        <li className="border-b border-gray-600">
+          Made with <i className="far fa-heart"></i> by Mannuel Ferreira in
+          Norway
         </li>
-        <li className="border-b-2 border-gray-600">
+        <li className="border-b border-gray-600">
           Fonts:{" "}
           <a
             href="https://fonts.adobe.com/fonts/freight-text#fonts-section"
@@ -19,7 +20,7 @@ const FooterSocial = () => {
             Freight
           </a>
         </li>
-        <li className="border-b-2 border-gray-600">
+        <li className="border-b border-gray-600">
           Icons:{" "}
           <a
             href="https://fontawesome.com"
@@ -35,4 +36,4 @@ const FooterSocial = () => {
   );
 };
 
-export default FooterSocial;
+export default FooterCopyright;
