cc19a39ea43853d75e6399f82885cc4a7bc16045
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-06-30 18:08:40 +0200

    article page layout

--- src/components/Articles/Article.tsx
+++ src/components/Articles/Article.tsx
@@ -22,15 +22,19 @@ const Article = ({ match }) => {
     <Fragment>
       <section className="app-row pt-10 pb-10 pr-5 pl-5">
         <div className="container mx-auto">
-          <h1 className="article-header text-5xl">Articles</h1>
+          <h1 className="article-header text-5xl">{post.title.rendered}</h1>
+          <div className="flex flex-wrap">
+            <article className="w-full sm:w-full md:w-full lg:flex-1 xl:flex-1 text-left mt-10 pr-10">
+              <div
+                className="break-words"
+                dangerouslySetInnerHTML={createMarkup(post.content.rendered)}
+              ></div>
+            </article>
+            <aside className="w-full sm:w-1/2 md:w-1/3 lg:1/3 xl:1/3 text-left mt-10 pl-10 border-solid border-gray-400 border-l">
+              <h1 className="text-3xl">...</h1>
 
-          <article className="text-left pt-10">
-            <h2 className="text-3xl">{post.title.rendered}</h2>
-            <div
-              className="break-words"
-              dangerouslySetInnerHTML={createMarkup(post.content.rendered)}
-            ></div>
-          </article>
+            </aside>
+          </div>
         </div>
       </section>
     </Fragment>
