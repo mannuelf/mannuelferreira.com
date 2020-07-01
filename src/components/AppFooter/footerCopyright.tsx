66d7a5f6ab182ef61be5e94854805d0f90d56d58
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-07-01 17:19:53 +0200

    footer footer

--- src/components/AppFooter/footerCopyright.tsx
+++ src/components/AppFooter/footerCopyright.tsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const FooterCopyright = () => {
   return (
-    <div className="w-full sm:w-full md:w-full lg:flex-1 xl:flex-1 text-left">
+    <div className="w-full sm:w-full md:w-full lg:flex-1 xl:flex-1 pb-10 text-left">
       <h3 className="text-2xl pb-3">Thank you for visiting</h3>
       <ul className="footer-copyright leading-8">
         <li className="border-b border-gray-600">
-          Made with <i className="far fa-heart"></i> by Mannuel Ferreira in
-          Norway
+          Made with <i className="far fa-heart"></i> by Mannuel Ferreira.
         </li>
         <li className="border-b border-gray-600">
           Fonts:{" "}
--- src/components/AppFooter/footerSocial.tsx
+++ src/components/AppFooter/footerSocial.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FooterSocial = () => {
   return (
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:1/3 xl:1/3 text-left pl-10">
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:1/3 xl:1/3 text-left sm:pl-0 md:pl-0 lg:pl-10">
       <h3 className="text-2xl pb-3">Follow me</h3>
       <ul className="flex">
         <li className="pr-4">
